0464c8f729df46592501cfd48491fc1c97d07ea0
Vicky Leong <vleong2332@gmail.com>
2017-07-13 18:51:05 -0400

    NOT DONE: Customize home page sections

--- wp-content/themes/illdy-child/functions.php
+++ wp-content/themes/illdy-child/functions.php
@@ -1,8 +1,6 @@
 <?php
-
 function my_theme_enqueue_styles() {
-
-    $parent_style = 'illdy';
+    $parent_style = 'illdy'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
 
     wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'illdy-child',
@@ -11,7 +9,11 @@ function my_theme_enqueue_styles() {
         wp_get_theme()->get('Version')
     );
 }
-
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
-
 ?>
+<?php
+
+// BEGIN ENQUEUE PARENT ACTION
+// AUTO GENERATED - Do not modify or remove comment markers above or below:
+
+// END ENQUEUE PARENT ACTION

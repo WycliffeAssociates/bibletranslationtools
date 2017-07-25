841a819842f34e46523d7c4b3fe6b5f3e67cdc0a
vleong2332 <vleong2332@gmail.com>
2017-07-25 17:04:20 -0400

    Display resources (first pass)

--- wp-content/themes/illdy-child/functions.php
+++ wp-content/themes/illdy-child/functions.php
@@ -13,18 +13,26 @@ function illdy_child_enqueue_styles() {
       array( $parent_style ),
       wp_get_theme()->get('Version')
     );
+    if (is_page('resources')) {
+      wp_enqueue_style( 'jquery-accordion',
+        $child_theme_dir . '/layout/css/jquery-ui.min.css',
+        array( $parent_style )
+      );
+    }
 }
 add_action( 'wp_enqueue_scripts', 'illdy_child_enqueue_styles' );
 
 function illdy_child_enqueue_scripts()
 {
-    wp_register_script( 'resources-page',
-      get_stylesheet_directory_uri() . '/layout/js/resources_page.js',
-      array( 'jquery' ),
-      NULL,
-      true
-    );
-    wp_enqueue_script( 'resources-page' );
+    if (is_page('resources')) {
+      wp_register_script( 'resources-page',
+        get_stylesheet_directory_uri() . '/layout/js/resources_page.js',
+        array( 'jquery', 'jquery-ui-accordion' ),
+        NULL,
+        true
+      );
+      wp_enqueue_script( 'resources-page' );
+    }
 
     // Use same handle as parent theme to override ('illdy-plugins')
     // (leongv) This fixes the problem with counter section on the front page where
--- wp-content/themes/illdy-child/layout/js/resources_page.js
+++ wp-content/themes/illdy-child/layout/js/resources_page.js
@@ -1,34 +1,71 @@
 var ResourcesPage = ( function( window, $, undefined ) {
 
-  var _oldCatalogUrl = 'https://api.door43.org/v3/catalog.json';
-  var _newCatalogUrl = 'https://api.unfoldingword.org/uw/txt/2/catalog.json';
-
-  var _getOldCatalog = function($, url) {
-    $.get(url, function() {
-      console.log('getting', url);
-    })
-    .done(function(a) {
-      console.log('done', a);
-    })
-    .fail(function(a) {
-      console.log('fail', a);
-    })
-    .always(function(a) {
-      console.log('fail', a);
-    });
-  };
-
   function ResourcesPage() {
 
-    var oldCatalog;
-    var newCatalog;
+    var container = undefined;
+    var resourcesData = undefined;
 
-    this.init = function() {
-      _getOldCatalog($, _oldCatalogUrl);
+    this.init = function(containerElId) {
+
+      // Get container
+      container = document.querySelector(containerElId);
+      if (!container) {
+        console.error('container ' + containerElId + ' for resources cannot be found');
+        return;
+      }
+
+      // Get json data and return a promise
+      return $.get('/wp-content/themes/illdy-child/data/resources.json', function(data) {
+        if (!data) {
+          console.error('resources.json doesn\'t provide any data');
+          return;
+        }
+        resourcesData = data;
+      });
     }
 
-    this.test = function() {
-      console.log('this is a test for ResourcesPages', window, $, _oldCatalogUrl, _newCatalogUrl);
+    this.renderResources = function(data) {
+      // Accordion root
+      $(container).append('<div id="accordion"></div>');
+      var accordion = $(container).find('#accordion');
+
+      JSON.parse(data).forEach(function(lang) {
+        // Section title
+        accordion.append('<h3 class="lang-code">' + lang.code + '</h3>');
+
+        accordion.append('<div id="' + lang.code + '-container" class="resource-container"></div>');
+        var langResourceContainer = accordion.find('#' + lang.code + '-container');
+
+        lang.resources.forEach(function(res) {
+          langResourceContainer.append('<h4 class="resource-title">' + res.name + '</h4>');
+
+          res.links && res.links.forEach(function(l) {
+            langResourceContainer.append('<a href="' + l.url + '">' + l.url + '</a>');
+          });
+
+          res.content.forEach(function(c, index) {
+            if (c.links) {
+              langResourceContainer.append('<p id="' + lang.code + '-' + res.slug + '-' + c.slug + '" class="resource-content">' + c.title + '</p>');
+
+              c.links.forEach(function(l) {
+                langResourceContainer.find('#' + lang.code + '-' + res.slug + '-' + c.slug).append('<span class="resource-link"><a href="' + l.url + '">' + l.format + '</a>');
+              });
+            }
+          });
+
+        });
+
+      });
+
+      // Initialize jQuery accordion
+      accordion.accordion({
+        collapsible: true,
+        heightStyle: 'content'
+      });
+    };
+
+    this.handleFailedInit = function(err) {
+      console.error('Some error happened.', err);
     };
 
   }
